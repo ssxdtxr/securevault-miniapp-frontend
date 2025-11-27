21f21644e9175655e945cb625073786a263e077f
ssxdtxr <tereskinegor68@gmail.com>
2025-11-27 12:26:50 +0300

    feat: add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,40 @@ import { useEffect, useMemo } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useMutation } from '@tanstack/react-query'
+import { api } from './shared/api/api'
+
+type TelegramUserType = {
+  telegram_id: string
+  username?: string | null
+  name?: string | null
+}
 
 function App() {
   const tg = window.Telegram?.WebApp
   const params = useMemo(() => new URLSearchParams(window.location.search), [])
+
+  const username = params.get('username')
+  const name = params.get('name')
+  const telegram_id = params.get('tg_id') as string
+
+  const {
+    mutateAsync: sync,
+    isPending,
+    isError,
+  } = useMutation({
+    mutationKey: ['sync'],
+    mutationFn: async (body: TelegramUserType) => await api.post('/users/sync', body),
+  })
 
   useEffect(() => {
     if (tg) {
       tg.ready()
       tg.requestFullscreen()
     }
-  }, [tg])
 
-  const username = params.get('username')
-  const id = params.get('tg_id')
+    sync({ telegram_id, name, username })
+  }, [name, sync, telegram_id, tg, username])
 
   return (
     <>
@@ -29,7 +49,15 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>
-        Привет, {username} {id}
+        {isPending ? (
+          <div>LOADING...</div>
+        ) : (
+          <p>
+            Привет, {username} {telegram_id}
+          </p>
+        )}
+
+        {isError && isError}
       </h2>
     </>
   )
